c448c62ea137ddd99b8a266ffb73b67e7a16d0a5
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-09-02 19:56:24 -0300

    feat: Adding more http verbs to controller

--- simple-book-api/src/main/kotlin/org/felipe/controllers/BookController.kt
+++ simple-book-api/src/main/kotlin/org/felipe/controllers/BookController.kt
@@ -1,16 +1,14 @@
 package org.felipe.controllers
 
-import jakarta.ws.rs.Path
-import jakarta.ws.rs.Produces
-import jakarta.ws.rs.core.MediaType
-import jakarta.ws.rs.core.Response
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
 import jakarta.ws.rs.PathParam
-import jakarta.ws.rs.PUT
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
 import jakarta.ws.rs.DELETE
-import jakarta.transaction.Transactional
 import org.felipe.models.Livro
 import org.felipe.service.LivrosService
 
@@ -20,17 +18,31 @@ import org.felipe.service.LivrosService
 class LivroResource(val livrosService: LivrosService) {
 
     @GET
-    fun listarLivros(): List<Livro> = livrosService.listAllLivros()
-
-    @GET
     @Path("/test")
-    fun test() : String {
+    fun test(): String {
         return "Hello world"
     }
 
+    @GET fun listarLivros(): List<Livro> = livrosService.listAllLivros()
+
     @GET
     @Path("/{id}")
-    fun findById(@PathParam("id") id: Long) : Response {
-        val livro = livrosService.find
+    fun findById(@PathParam("id") id: Long): Response {
+        val livro = livrosService.findLivrosById(id)
+        return if (livro != null) Response.ok(livro).build()
+        else Response.status(Response.Status.NOT_FOUND).build()
+    }
+
+    @POST
+    fun addLivro(livro: Livro): Response {
+        livrosService.addLivro(livro)
+        return Response.status(Response.Status.CREATED).build()
+    }
+
+    @DELETE
+    @Path("/{id}")
+    fun deleteLivro(@PathParam("id") id: Long): Response{
+        livrosService.deleteLivro(id)
+        return Response.status(Response.Status.NO_CONTENT).build()
     }
 }
