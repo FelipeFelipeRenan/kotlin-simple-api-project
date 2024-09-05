1b67bf292c7869737b1ca9256bc4485a0721b953
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-09-05 19:43:04 -0300

    fix: Fixing books persistence and adding swagger

--- simple-book-api/src/main/kotlin/org/felipe/service/LivroService.kt
+++ simple-book-api/src/main/kotlin/org/felipe/service/LivroService.kt
@@ -2,22 +2,23 @@ package org.felipe.service
 
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.transaction.Transactional
-import org.felipe.repositories.LivrosRepository
 import org.felipe.models.Livro
+import org.felipe.repositories.LivrosRepository
 
 @ApplicationScoped
-class LivrosService(val livrosRepository: LivrosRepository){
+class LivrosService(val livrosRepository: LivrosRepository) {
     fun listAllLivros(): List<Livro> = livrosRepository.listAll()
 
-    fun findLivrosById(id : Long): Livro? = livrosRepository.findById(id)
+    fun findLivrosById(id: Long): Livro? = livrosRepository.findById(id)
 
     @Transactional
-    fun addLivro(livro: Livro){
-        livrosRepository.persist(livro)
+    fun addLivro(livro: Livro) {
+        if (livro.id == null) {
+            livrosRepository.persist(livro)
+        }
     }
-
     @Transactional
-    fun deleteLivro(id: Long){
+    fun deleteLivro(id: Long) {
         livrosRepository.deleteById(id)
     }
 }
